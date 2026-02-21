83076154362675295b1b4131369e2e0a60e38836
Soham <sohamkagrawal@gmail.com>
2026-02-21 10:12:57 -0500

    updating a polished mesh rendering pipeline

--- data-processing/mesh-rendering.py
+++ data-processing/mesh-rendering.py
@@ -93,5 +93,87 @@ def render_full_heart(nifti_path):
 #     label_value=8,  # 1 = LV
 #     color="red"
 # )
-render_full_heart("cropped/cropped/pat0_cropped_seg.nii.gz")
+
+#polished version of the render heart
+def render_polished_heart(nifti_path):
+
+    seg = nib.load(nifti_path)
+    seg_data = seg.get_fdata()
+    spacing = seg.header.get_zooms()
+
+    label_info = {
+        1: ("LV", "red"),
+        2: ("RV", "blue"),
+        3: ("LA", "pink"),
+        4: ("RA", "purple"),
+        5: ("Aorta", "yellow"),
+        6: ("PA", "green"),
+        7: ("SVC", "orange"),
+        8: ("IVC", "brown")
+    }
+
+    plotter = pv.Plotter()
+    actors = {}
+
+    for label_value, (name, color) in label_info.items():
+
+        binary_mask = (seg_data == label_value).astype(np.uint8)
+
+        if np.sum(binary_mask) == 0:
+            continue
+
+        verts, faces, normals, values = measure.marching_cubes(
+            binary_mask,
+            level=0.5,
+            spacing=spacing
+        )
+
+        faces = np.hstack(
+            [np.full((faces.shape[0], 1), 3), faces]
+        ).astype(np.int64)
+
+        mesh = pv.PolyData(verts, faces)
+
+       #smoothening
+        mesh = mesh.smooth(n_iter=30, relaxation_factor=0.1)
+
+        actor = plotter.add_mesh(
+            mesh,
+            color=color,
+            opacity=0.6,
+            specular=0.5,
+            specular_power=15,
+            smooth_shading=True,
+            name=name
+        )
+
+        actors[name] = actor
+
+        #add definition to the edges
+        plotter.add_mesh(mesh, style="wireframe", color="black", opacity=0.1)
+
+    plotter.add_axes()
+    plotter.enable_lightkit()
+
+    #Hovering interaction (currently works on right click)
+    def on_pick(mesh):
+        if mesh is None:
+            return
+
+        picked_name = mesh.actor.GetName()
+
+        for name, actor in actors.items():
+            if name == picked_name:
+                actor.prop.opacity = 1.0
+            else:
+                actor.prop.opacity = 0.2
+
+        plotter.render()
+
+    plotter.enable_mesh_picking(callback=on_pick, use_actor=True)
+
+    plotter.show()
+    
+# render_full_heart("cropped/cropped/pat0_cropped_seg.nii.gz")
+render_polished_heart("cropped/cropped/pat0_cropped_seg.nii.gz")
 
